88f34972d08fff4f31c1039564e2ecc9a0572f51
Billy Julian Lesmana <hunter140797@gmail.com>
2023-02-02 23:58:59 +0100

    fixes the mouse cursor lock behaviour on the main menu

--- Assets/Scripts/Menu/ScreenHandler.cs
+++ Assets/Scripts/Menu/ScreenHandler.cs
@@ -7,11 +7,33 @@ public class ScreenHandler : StateHandler
     [SerializeField] private CanvasGroup canvasGroup;
     public override string Name => handlerName;
 
+    private GameObject player;
+
+    public MenuStateMachine stateMachine;
+
     public override void OnEnter<T>(T transition)
     {
         canvasGroup.alpha = 1;
         canvasGroup.interactable = true;
         canvasGroup.blocksRaycasts = true;
+
+        if (stateMachine.CurrentState == stateMachine.MainMenuHandler)
+        {
+            Debug.Log("current state main menu");
+            Time.timeScale = 0;
+            player.GetComponent<StarterAssetsInputs>().cursorLocked = false;
+            Cursor.lockState = CursorLockMode.None;
+            player.GetComponent<StarterAssetsInputs>().cursorInputForLook = false;
+        }
+        else if (stateMachine.CurrentState == stateMachine.StartGameHandler)
+        {
+            Debug.Log("current state play game");
+            Time.timeScale = 1;
+            player.GetComponent<StarterAssetsInputs>().cursorLocked = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            player.GetComponent<StarterAssetsInputs>().cursorInputForLook = true;
+        }
+
     }
 
     public override void OnExit<T>(T transition)
@@ -19,15 +41,17 @@ public class ScreenHandler : StateHandler
         canvasGroup.alpha = 0;
         canvasGroup.interactable = false;
         canvasGroup.blocksRaycasts = false;
-        
-    }   
+
+    }
 
     private void Awake()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         OnEnter(MenuTransitions.MainMenuSelected);
-    } 
+    }
 
-    public void PlayerDeath() {
+    public void PlayerDeath()
+    {
         OnEnter(MenuTransitions.PlayerDead);
     }
 }
